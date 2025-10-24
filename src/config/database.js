73b81662760e8d959cb33f92a8023fadb48ef7cc
Davi Alves <davi_ti@outlook.com>
2025-10-24 16:01:56 -0300

    adicionado timeout

--- src/config/database.js
+++ src/config/database.js
@@ -13,8 +13,8 @@ export const connectDB = async () => {
 
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000, // Aumenta para 30 segundos (exemplo)
+      socketTimeoutMS: 45000, // Aumenta timeout do socket
     });
 
     isConnected = conn.connections[0].readyState === 1;
