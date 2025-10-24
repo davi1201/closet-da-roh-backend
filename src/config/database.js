0c938a7d8f31cc085517f6231f8a820ec576ab88
Davi Alves <davi_ti@outlook.com>
2025-10-24 15:11:01 -0300

    removido timout

--- src/config/database.js
+++ src/config/database.js
@@ -15,8 +15,6 @@ export const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 30000,
-      socketTimeoutMS: 45000,
     });
 
     isConnected = conn.connections[0].readyState === 1;
